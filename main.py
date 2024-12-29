a02f1fcfa1b09b4d512b0e37c53fe12906080e2f
Baptiste P <baptiste.pauchet13@gmail.com>
2024-12-29 18:49:20 +0100

    Ajout listen

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from objet.arrera_voice import *
 
 def main():
-    av = CArreraVoice()
+    av = CArreraVoice(jsonWork("conf/sixConfig.json"))
     av.say("Bonjour, je suis Arrera, votre assistant personnel")
+    print("Ecoute en cours...")
+    av.listen()
+    print(av.getTextMicro())
 
 if __name__ == "__main__":
     main()
--- objet/arrera_voice.py
+++ objet/arrera_voice.py
@@ -2,11 +2,22 @@ from gtts import gTTS as gt
 from playsound3 import playsound as pl
 import os
 import threading as th
+from librairy.travailJSON import jsonWork
+import speech_recognition as sr
 
 
 class CArreraVoice:
-    def __init__(self):
-        pass
+    def __init__(self,configFile:jsonWork):
+        self.__configFile = configFile
+        self.__emplacementSoundMicro = self.__configFile.lectureJSON("fileMicro")
+        self.__soundMicro = True
+        self.__outPutText = ""
+
+    def loadConfig(self):
+        if (self.__configFile.lectureJSON("soundMicro") == 1 ):
+            self.__soundMicro = True
+        else:
+            self.__soundMicro = False
 
     def say(self,text:str):
         tts = gt(text, lang='fr')
@@ -22,3 +33,23 @@ class CArreraVoice:
 
     def playFile(self,file:str):
         pl(file)
+
+    def listen(self):
+        if self.__soundMicro:
+            pl(self.__emplacementSoundMicro)
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='fr-FR')
+            self.__outPutText = text
+            return 0
+        except sr.UnknownValueError:
+            return -1
+        except sr.RequestError as e:
+            return -2
+
+    def getTextMicro(self):
+        return self.__outPutText
